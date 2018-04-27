42c95478ff0d991925d739041411545a4463bdfb
nerincon <nerincon1@gmail.com>
2018-04-26 19:48:22 -0500

    start login

---
+++ app.py
@@ -0,0 +1,53 @@
+import tornado.ioloop
+import tornado.web
+import tornado.log
+
+import os
+
+
+from jinja2 import \
+  Environment, PackageLoader, select_autoescape
+
+ENV = Environment(
+  loader=PackageLoader('myapp', 'templates'),
+  autoescape=select_autoescape(['html', 'xml'])
+)
+
+class TemplateHandler(tornado.web.RequestHandler):
+  def render_template (self, tpl, context):
+    template = ENV.get_template(tpl)
+    self.write(template.render(**context))
+
+
+class LoginHandler(TemplateHandler):
+  def get(self, page='login'):
+    page = page + '.html'
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    self.render_template(page, {})
+
+
+def make_app():
+  return tornado.web.Application([
+    (r"/", LoginHandler),
+    (
+      r"/static/(.*)",
+      tornado.web.StaticFileHandler,
+      {'path': 'static'}
+    ),
+  ], autoreload=True)
+
+
+
+
+
+if __name__ == "__main__":
+  tornado.log.enable_pretty_logging()
+  
+  app = make_app()
+  PORT = int(os.environ.get('PORT', '8000'))
+  app.listen(PORT)
+  tornado.ioloop.IOLoop.current().start()
+
+
